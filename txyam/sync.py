6356c1ef68b2b7016ec7325d8fa9c25226082ae5
weykent <weykent@weasyl.com>
2014-05-23 20:51:08 -0700

    Add async_operation to the synchronous client.
    This is useful for when you don't care if an operation succeeds or whatever;
    you just want it to run eventually.

--- txyam/sync.py
+++ txyam/sync.py
@@ -13,6 +13,10 @@ class SynchronousYamClient(object):
     def operation(self, operation, *a, **kw):
         return getattr(self.yamClient, operation)(*a, **kw)
 
+    @wait_for(timeout=0.1)
+    def async_operation(self, operation, *a, **kw):
+        getattr(self.yamClient, operation)(*a, **kw)
+
     @run_in_reactor
     def disconnect(self):
         self.yamClient.disconnect()
